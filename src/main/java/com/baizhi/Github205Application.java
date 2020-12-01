cdbfa67ae51c579373519967d253f22d866a3425
zhangxh <zhang_xh624@163.com>
2020-12-01 20:02:26 +0800

    zxh第一次修改测试类

---
+++ src/main/java/com/baizhi/Github205Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github205Application.class, args);
+    }
+
+}
